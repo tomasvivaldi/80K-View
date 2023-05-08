03e71cd5904aaee79373067ffdc52e0e63da3886
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-05-08 14:21:18 +0800

    Banner and Welcome updated

--- src/template/Banner.tsx
+++ src/template/Banner.tsx
@@ -7,28 +7,18 @@ function Banner() {
   return session ? (
     <div className="font-semibold rounded-lg shadow-lg
      bg-gradient-to-r from-cyan-400 via-teal-300 to-green-400 text-slate-800 
-     text-xs sm:text-base md:text-xl p-2 md:p-8 flex flex-col md:flex-row items-center justify-between">
+     text-xs sm:text-base md:text-4xl p-2 md:p-8 flex flex-col md:flex-row items-center justify-between">
       <div className='flex h-fit my-4'>
         <p>
-          It's time to  
-        </p>
-        <span className="relative mx-2 inline-block before:absolute before:-inset-1 before:block before:rounded-lg before:bg-blue-800">
-          <span className="relative px-1 text-white">
-          update
-          </span>
-        </span>
-        <p>
-        your life fulfilment tracker!
+          It's time to update your life fulfilment tracker!
         </p>
         
       </div>
       <div className='my-4'>
         <Link href="/forms" className="mx-auto my-16">
-          <button className='bg-blue-800 rounded-full py-2 px-8 border-2 text-gray-100 border-gray-200 hover:bg-blue-900 active:bg-blue-700'>Click here!</button>
+          <button className='text-xl w-64 bg-blue-300/20 rounded-full py-2 px-8 border-2 text-black border-gray-800 hover:bg-blue-400/20 active:bg-blue-300/20'>Update Now!</button>
         </Link>
-      </div>
-      <div />
-      
+      </div>      
     </div>
   ) : (
     <div className="">
--- src/template/Welcome.tsx
+++ src/template/Welcome.tsx
@@ -4,14 +4,9 @@ import React from 'react';
 function Welcome() {
   const { data: session } = useSession();
   return (
-    <div className="font-semibold text-slate-800 text-xl sm:text-2xl md:text-3xl my-8">
+    <div className="font-semibold text-slate-800 text-xl sm:text-2xl md:text-4xl my-8">
       <p>
-        Welcome back
-        <span className="relative mx-2 inline-block before:absolute before:-inset-1 before:block before:rounded-lg before:bg-blue-800 my-2">
-          <span className="relative px-1 text-white ">
-            {session?.user?.name}
-          </span>
-        </span>
+        Welcome back, {session?.user?.name}.
       </p>
     </div>
   );
