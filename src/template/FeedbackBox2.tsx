b260df6cfe2092708a85527c4b4ffee8081464ad
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-05-17 12:28:29 +0800

    fixed text sizing on smaller screens

--- src/template/FeedbackBox2.tsx
+++ src/template/FeedbackBox2.tsx
@@ -6,14 +6,14 @@ function FeedbackBox() {
   return session ? (
     <div className=" rounded-lg shadow-lg text-black md:w-[1550px]
      bg-white
-     text-xs sm:text-base  p-2 md:p-4 flex flex-col items-left ">
+       p-2 md:p-4 flex flex-col items-left ">
       <div className='flex h-fit mb-2'>
-        <p className='font-semibold md:text-lg'>
+        <p className='font-semibold lg:text-lg'>
           Improvement suggestion for this month
         </p>
       </div>
       <div className='text-sm md:text-base'>
-      <ol className='flex flex-col w-full list-inside list-[upper-roman] '>
+      <ol className='flex flex-col w-full list-inside list-[upper-roman] text-xs lg:text-sm xl:text-base'>
         <li>First item iputm lorem ipsum bla  item iputm lorem ipsum bla</li>
         <li>First item iputm lorem ipsum bla  item iputm lorem ipsum bla</li>
         <li>First item iputm lorem ipsum bla  item iputm lorem ipsum bla</li>
