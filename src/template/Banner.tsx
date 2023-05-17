c250e493a16ee58972bf11d78354011f8c7351f4
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-05-17 08:12:31 +0800

    fixed styling

--- src/template/Banner.tsx
+++ src/template/Banner.tsx
@@ -5,18 +5,18 @@ import React from 'react';
 function Banner() {
   const { data: session } = useSession();
   return session ? (
-    <div className="font-semibold rounded-lg shadow-lg
+    <div className="font-semibold rounded-lg shadow-lg my-4
      bg-gradient-to-r from-cyan-400 via-teal-300 to-green-400 text-slate-800 
-     text-xs sm:text-base md:text-4xl p-2 md:p-8 flex flex-col md:flex-row items-center justify-between">
+      sm:text-base  p-2 md:p-8 md:pr-16 flex flex-col md:flex-row h-20 items-center justify-between">
       <div className='flex h-fit my-4'>
-        <p>
+        <p className='text-xl'>
           It's time to update your life fulfilment tracker!
         </p>
         
       </div>
       <div className='my-4'>
         <Link href="/forms" className="mx-auto my-16">
-          <button className='text-xl w-64 bg-blue-300/20 rounded-full py-2 px-8 border-2 text-black border-gray-800 hover:bg-blue-400/20 active:bg-blue-300/20'>Update Now!</button>
+          <button className='text-lg w-44 bg-blue-300/20 rounded-full p-1 border-2 text-black border-gray-800 hover:bg-blue-400/20 active:bg-blue-300/20'>Update Now!</button>
         </Link>
       </div>      
     </div>
