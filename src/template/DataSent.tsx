dc8d3461732f55a5e32c992af6164f36176b7428
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-05-18 08:17:07 +0800

    Updated sizing for small screens

--- src/template/DataSent.tsx
+++ src/template/DataSent.tsx
@@ -7,12 +7,12 @@ interface BeginFormProps {}
 
 const BeginForm: React.FC<BeginFormProps> = () => {
   return (
-    <div className="flex w-full flex-col gap-4 rounded-md border-gray-200 bg-white px-4 py-5">
+    <div className="flex flex-col gap-4 rounded-md border-gray-200 bg-white px-4 py-5 sm:w-[80%] w-full mx-auto">
       <div className="my-4 w-full text-center text-2xl font-semibold text-gray-800">
         Congratulations!!!
       </div>
       {/* Success Icon */}
-      <div className=" mx-auto w-fit space-y-4">
+      <div className=" w-fit space-y-4 mx-8 sm:mx-24">
         <p>Thanks for sharing your lifestyle info! We encourage you to check back next month.
            You'll find tables and graphs where you can track your progress, see how you've improved 
            and where to focus next. Looking forward to your return!</p>
