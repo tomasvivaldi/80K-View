1931b6a702a2060fee46338aa89dd3000d96106c
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-05-25 13:12:43 +0800

    Functional Cards

--- src/template/Stats.tsx
+++ src/template/Stats.tsx
@@ -1,6 +1,99 @@
 import { StatsCard } from '@/stats/StatsCard';
 
-const Stats = () => (
+type Stats = {
+  data?: UserDataById;
+}
+
+
+
+const percentChange = (oldValue: number, newValue: number) => {
+  if(oldValue === 0){
+    return ((newValue - oldValue) / (oldValue + 1)) * 100;
+  }
+  return ((newValue - oldValue) / oldValue) * 100;
+};
+
+const calculateStats = (data: UserDataById) => {
+  let highestScoreCategory = null;
+  let highestScore = -Infinity;
+  let highestScorePercentChange = 0;
+
+  let lowestScoreCategory = null;
+  let lowestScore = Infinity;
+  let lowestScorePercentChange = 0;
+
+  let highestIncreaseCategory = null;
+  let highestIncrease = -Infinity;
+
+  let highestDropCategory = null;
+  let highestDrop = Infinity;
+
+  for (let category in data) {
+    if (Array.isArray(data[category as keyof UserDataById])) {
+      let entries = data[category as keyof UserDataById] as Category[];
+      console.log('entriesentriesentries',entries)
+      if (Array.isArray(entries) && entries.length >= 2) {
+        let lastEntry = entries[0]?.score;
+        let secondLastEntry = entries[1]?.score;
+        if (lastEntry !== undefined && secondLastEntry !== undefined) {
+          let changePercent = percentChange(secondLastEntry, lastEntry);
+
+          if (lastEntry > highestScore) {
+            highestScore = lastEntry;
+            highestScoreCategory = category;
+            highestScorePercentChange = changePercent;
+          }
+
+          if (lastEntry < lowestScore) {
+            lowestScore = lastEntry;
+            lowestScoreCategory = category;
+            lowestScorePercentChange = changePercent;
+          }
+
+          if (changePercent > highestIncrease) {
+            highestIncrease = changePercent;
+            highestIncreaseCategory = category;
+          }
+
+          if (changePercent < highestDrop) {
+            highestDrop = changePercent;
+            highestDropCategory = category;
+          }
+        }
+      }
+    }
+  }
+
+  return {
+    highestScoreCategory,
+    highestScorePercentChange,
+    lowestScoreCategory,
+    lowestScorePercentChange,
+    highestIncreaseCategory,
+    highestIncrease,
+    highestDropCategory,
+    highestDrop,
+  };
+};
+
+const formatCategory = (category: string | null) => {
+  if (!category) {
+    return "";
+  }
+  
+  return category
+    .split('_')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' / ');
+}
+
+const Stats = ({ data }: Stats) => {
+  if (!data) return null;
+
+  const stats = calculateStats(data);
+
+return(
+
   <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-4">
     <StatsCard
       icon={
@@ -16,9 +109,11 @@ const Stats = () => (
           <path d="M3 21v-2a4 4 0 014-4h4a4 4 0 014 4v2M16 3.13a4 4 0 010 7.75M21 21v-2a4 4 0 00-3-3.85" />
         </svg>
       }
-      text="Overall Score"
+      text="Highest Score Category"
     >
-      34% Increase
+      <p>
+      {formatCategory(stats.highestScoreCategory)}
+      </p>
     </StatsCard>
     <StatsCard
       icon={
@@ -35,9 +130,9 @@ const Stats = () => (
           <path d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2" />
         </svg>
       }
-      text="Best category this month"
+      text="Lowest Score Category"
     >
-      *Category Name*
+      {formatCategory(stats.lowestScoreCategory)}
     </StatsCard>
     <StatsCard
       icon={
@@ -55,9 +150,16 @@ const Stats = () => (
           <path d="M6 5l14 1-1 7H6" />
         </svg>
       }
-      text="Biggest score drop"
+      text="Highest Increase Category"
     >
-      23% - *Category Name*
+      <div className='flex flex-row gap-1 items-center'>
+        <p>
+        {formatCategory(stats.highestIncreaseCategory)}, {stats.highestIncrease}% 
+        </p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" style={{ width:"20", height:"20"}}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5" stroke="green"/>
+        </svg>
+      </div>
     </StatsCard>
     <StatsCard
       icon={
@@ -72,11 +174,18 @@ const Stats = () => (
           <path d="M4 21V8a3 3 0 013-3h10a3 3 0 013 3v6a3 3 0 01-3 3H8l-4 4M8 9h8M8 13h6" />
         </svg>
       }
-      text="Worst category"
+      text="Highest Increase Category"
     >
-     *Category Name* 
+      <div className='flex flex-row gap-1 items-center'>
+        <p>
+        {formatCategory(stats.highestDropCategory)}, {stats.highestDrop}% 
+        </p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" style={{ width:"20", height:"20"}}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" stroke="red"/>
+        </svg>
+      </div>
     </StatsCard>
   </div>
-);
+)} ;
 
 export { Stats };
