4ffeec916c26976637cf6aa6ec6b350e75e1edb3
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-07-19 21:20:03 +0700

    included new cases

--- src/pages/api/stripe/stripe-webhook.ts
+++ src/pages/api/stripe/stripe-webhook.ts
@@ -31,6 +31,34 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
       case 'payment_intent.succeeded':
         const paymentIntent = event.data.object as Stripe.PaymentIntent;
         console.log('PaymentIntent was successful!', paymentIntent);
+
+
+  // // Extract the customer's email. Please note you need to make sure the customer's email is associated 
+  // // with the payment intent data in your Stripe checkout process.
+  // const customerEmail = paymentIntent.receipt_email; 
+
+  // // Update the user in the database.
+  // if (customerEmail) {
+  //   try {
+  //     const user = await User.findOneAndUpdate(
+  //       { email: customerEmail },
+  //       { isActive: true },
+  //       { new: true }  // This option asks Mongoose to return the updated user model.
+  //     );
+  
+  //     if (user) {
+  //       console.log(`User subscription status updated!`, user);
+  //     } else {
+  //       console.error(`No user found with email: ${customerEmail}`);
+  //     }
+  //   } catch (err) {
+  //     console.error(`Failed to update user subscription status: ${err}`);
+  //   }
+  // }
+
+  // console.log('PaymentIntent was successful!', paymentIntent);
+
+
         break;
       case 'charge.succeeded':
         const charge = event.data.object as Stripe.Charge;
@@ -40,6 +68,28 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         const paymentIntentCreated = event.data.object as Stripe.PaymentIntent;
         console.log('PaymentIntent was created!', paymentIntentCreated);
         break;
+
+      case 'customer.subscription.created':
+        const subscriptionCreated = event.data.object as Stripe.Subscription;
+        console.log('Subscription was created!', subscriptionCreated);
+        // set isActive to true
+        break;
+      case 'customer.subscription.updated':
+        const subscriptionUpdated = event.data.object as Stripe.Subscription;
+        console.log('Subscription was updated!', subscriptionUpdated);
+        // update isActive based on the new subscription status
+        break;
+      case 'customer.subscription.deleted':
+        const subscriptionDeleted = event.data.object as Stripe.Subscription;
+        console.log('Subscription was deleted!', subscriptionDeleted);
+        // set isActive to false
+        break;
+      case 'invoice.payment_failed':
+        const invoice = event.data.object as Stripe.Invoice;
+        console.log('Invoice payment failed!', invoice);
+        // set isActive to false
+        break;  
+
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
