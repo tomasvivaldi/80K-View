62b93e022469379f43b4ed96b5256ea2e55f04f2
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-07-20 12:06:01 +0700

    update database user state

--- src/pages/api/stripe/stripe-webhook.ts
+++ src/pages/api/stripe/stripe-webhook.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { buffer } from 'micro';
 import Stripe from 'stripe';
+import { UPDATE_USER_SUBSCRIPTION } from 'graphql/mutations';
+import { useMutation } from '@apollo/client';
+
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2022-11-15',
@@ -33,30 +36,30 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         console.log('PaymentIntent was successful!', paymentIntent);
 
 
-  // // Extract the customer's email. Please note you need to make sure the customer's email is associated 
-  // // with the payment intent data in your Stripe checkout process.
-  // const customerEmail = paymentIntent.receipt_email; 
+      // Extract the customer's email. Please note you need to make sure the customer's email is associated 
+      // with the payment intent data in your Stripe checkout process.
+      const customerEmail = paymentIntent?.receipt_email; 
 
-  // // Update the user in the database.
-  // if (customerEmail) {
-  //   try {
-  //     const user = await User.findOneAndUpdate(
-  //       { email: customerEmail },
-  //       { isActive: true },
-  //       { new: true }  // This option asks Mongoose to return the updated user model.
-  //     );
-  
-  //     if (user) {
-  //       console.log(`User subscription status updated!`, user);
-  //     } else {
-  //       console.error(`No user found with email: ${customerEmail}`);
-  //     }
-  //   } catch (err) {
-  //     console.error(`Failed to update user subscription status: ${err}`);
-  //   }
-  // }
 
-  // console.log('PaymentIntent was successful!', paymentIntent);
+      const [updateUserSubscription] = useMutation(UPDATE_USER_SUBSCRIPTION);
+
+
+      // Update the user in the database.
+      if (customerEmail) {
+        try {
+          const user = await updateUserSubscription({ variables: { email: customerEmail, isActive: true } });
+      
+          if (user) {
+            console.log(`User subscription status updated!`, user);
+          } else {
+            console.error(`No user found with email: ${customerEmail}`);
+          }
+        } catch (err) {
+          console.error(`Failed to update user subscription status: ${err}`);
+        }
+      }
+
+      console.log('PaymentIntent was successful!', paymentIntent);
 
 
         break;
@@ -64,6 +67,9 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         const charge = event.data.object as Stripe.Charge;
         console.log('Charge was successful!', charge);
         break;
+
+
+
       case 'payment_intent.created':
         const paymentIntentCreated = event.data.object as Stripe.PaymentIntent;
         console.log('PaymentIntent was created!', paymentIntentCreated);
