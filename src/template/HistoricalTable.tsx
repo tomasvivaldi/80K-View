4cfe1c59cdaadbbfa1fa799243c2ca67c6d227cd
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-07-31 23:55:10 +0700

    Lock gradient

--- src/template/HistoricalTable.tsx
+++ src/template/HistoricalTable.tsx
@@ -170,9 +170,11 @@ const HistoricalTable: React.FC<HistoricalTableProps> = ({ data }) => {
                             value !== undefined ? 
                             `${value.toFixed(1)}` : 
                             isFuture(parseInt(year), monthIndex) ? 
-                              <div className='border border-black px-2 py-1 rounded-full'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" className="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                              </svg></div>
+                              <div className=' shadow-lg border border-black px-2 py-1 rounded-full bg-gradient-to-r from-gray-100 to-stone-300/50'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" className="w-6 h-6">
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                                </svg>
+                              </div>
                             : isCurrentMonth(parseInt(year), monthIndex) ? 
                             <p className='text-black rounded-full px-2 py-1 font-semibold text-lg border-2 border-cyan-500 animate-bounce'>Update</p>
                             : ''
