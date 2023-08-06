4d617f9873ed332c562a85e5c40d83828540e736
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-08-06 20:46:25 +0700

    new fix

--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -7,31 +7,36 @@ import { useEffect, useState } from 'react';
 
 const Subscription = () => {
   const { data: session } = useSession();
-  console.log('session',session)
   const user_query = queries.GET_USER_BY_EMAIL
   const [userRef, setUserRef] = useState<string | null>(null);
-  const { loading, data } = useQuery<Record<string, any>>(user_query, {
+
+  const { loading, data, stopPolling } = useQuery<Record<string, any>>(user_query, {
     variables: { email: session?.user?.email },
+    pollInterval: 500, // re-run the query every 500ms
   });
   
   useEffect(() => {
-    
     if (!loading && data && data.userByEmail) {
       const userRef = data.userByEmail?.id;
       console.log('user_ref', userRef);
       setUserRef(userRef);
+      stopPolling(); // stop re-running the query once the user is fetched
     }
-  }, [loading, data]);
+  }, [loading, data, stopPolling]);
 
   if(!userRef){console.log('!userRef',userRef)}
-  
-  const stripeHTML = `
-    <stripe-pricing-table 
-    pricing-table-id="prctbl_1Nc0foIrrtDVRB0pQKFEmNjO" 
-    publishable-key="pk_test_51NNWTwIrrtDVRB0pW9iXxyK6xF3SbJdZ5Ry6hyHEFFDxYDsvajmy2o7inI9C7rnj9yOjyKzPWrejrDBGBP0MvuxM00Zhd5xpez"
-    client-reference-id=${userRef!}>
-    </stripe-pricing-table>
-  `;
+
+  let stripeHTML = ''; // initialize stripeHTML as an empty string
+
+  if(userRef){ // if userRef exists, then define stripeHTML
+    stripeHTML = `
+      <stripe-pricing-table 
+        pricing-table-id="prctbl_1Nc0foIrrtDVRB0pQKFEmNjO" 
+        publishable-key="pk_test_51NNWTwIrrtDVRB0pW9iXxyK6xF3SbJdZ5Ry6hyHEFFDxYDsvajmy2o7inI9C7rnj9yOjyKzPWrejrDBGBP0MvuxM00Zhd5xpez"
+        client-reference-id=${userRef!}>
+      </stripe-pricing-table>
+    `;
+  }
 
   return (
     <div className="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
