87f19aedd7c8dbb72bdb28cfb5fecce11f44f3d2
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-08-06 20:19:01 +0700

    fix

--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -1,38 +1,28 @@
-// import { useQuery } from '@apollo/client';
-// import { queries } from 'graphql/queries';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
-// import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from 'react';
 
 const Subscription = () => {
   const { data: session } = useSession();
   console.log('session',session)
-  // const user_query = queries.GET_USER_BY_EMAIL
-  // const [userRef, setUserRef] = useState<string | null>(null);
-  // const { loading, data } = useQuery<Record<string, any>>(user_query, {
-  //   variables: { email: session?.user?.email },
-  // });
   
-  // useEffect(() => {
-    
-  //   if (!loading && data && data.userByEmail) {
-  //     const userRef = data.userByEmail?.id;
-  //     console.log('user_ref', userRef);
-  //     setUserRef(userRef);
-  //   }
-  // }, [loading, data]);
+  const [stripeHTML, setStripeHTML] = useState('');
 
-  // if(!userRef){console.log('!userRef',userRef)}
+  useEffect(() => {
+    console.log('session', session);
+    if (session?.user?.email) {
+      setStripeHTML(`
+        <stripe-pricing-table 
+          pricing-table-id="prctbl_1Nc0foIrrtDVRB0pQKFEmNjO" 
+          publishable-key="pk_test_51NNWTwIrrtDVRB0pW9iXxyK6xF3SbJdZ5Ry6hyHEFFDxYDsvajmy2o7inI9C7rnj9yOjyKzPWrejrDBGBP0MvuxM00Zhd5xpez"
+          client-reference-id=${session.user.email}>
+        </stripe-pricing-table>
+      `);
+    }
+  }, [session]);
+
+  console.log("stripeHTML", stripeHTML);
   
-  const stripeHTML = `
-    <stripe-pricing-table 
-    pricing-table-id="prctbl_1Nc0foIrrtDVRB0pQKFEmNjO" 
-    publishable-key="pk_test_51NNWTwIrrtDVRB0pW9iXxyK6xF3SbJdZ5Ry6hyHEFFDxYDsvajmy2o7inI9C7rnj9yOjyKzPWrejrDBGBP0MvuxM00Zhd5xpez"
-    client-reference-id="69">
-    </stripe-pricing-table>
-  `;
-
   return (
     <div className="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
       <Head>
@@ -43,9 +33,7 @@ const Subscription = () => {
         <p className="text-gray-700 mb-4 max-w-md">
           We are on a mission to empower and improve people's life! When you sign up, we are giving you an <span className=' font-bold'>entire year</span> of <span className=' font-bold'>free subscription!</span>
         </p>
-        {session?.user?.email && session.user.email !== '' &&
         <div className='w-full' dangerouslySetInnerHTML={{ __html: stripeHTML }} />
-  }
         <div className="flex flex-col space-y-4">
           <p>OR</p>
           <button
