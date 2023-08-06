df37ded86321b431e56e6ce04a9122c4d5e18f08
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-08-06 20:25:18 +0700

    try new fix

--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -1,28 +1,20 @@
+"use client"
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 
 const Subscription = () => {
   const { data: session } = useSession();
   console.log('session',session)
   
-  const [stripeHTML, setStripeHTML] = useState('');
+  const stripeHTML = `
+    <stripe-pricing-table 
+    pricing-table-id="prctbl_1Nc0foIrrtDVRB0pQKFEmNjO" 
+    publishable-key="pk_test_51NNWTwIrrtDVRB0pW9iXxyK6xF3SbJdZ5Ry6hyHEFFDxYDsvajmy2o7inI9C7rnj9yOjyKzPWrejrDBGBP0MvuxM00Zhd5xpez"
+    client-reference-id=${session?.user?.email}>
+    </stripe-pricing-table>
+  `;
+  console.log("stripeHTML",stripeHTML)
 
-  useEffect(() => {
-    console.log('session', session);
-    if (session?.user?.email) {
-      setStripeHTML(`
-        <stripe-pricing-table 
-          pricing-table-id="prctbl_1Nc0foIrrtDVRB0pQKFEmNjO" 
-          publishable-key="pk_test_51NNWTwIrrtDVRB0pW9iXxyK6xF3SbJdZ5Ry6hyHEFFDxYDsvajmy2o7inI9C7rnj9yOjyKzPWrejrDBGBP0MvuxM00Zhd5xpez"
-          client-reference-id=${session.user.email}>
-        </stripe-pricing-table>
-      `);
-    }
-  }, [session]);
-
-  console.log("stripeHTML", stripeHTML);
-  
   return (
     <div className="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
       <Head>
