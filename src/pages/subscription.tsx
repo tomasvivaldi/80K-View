0cbbce94600f1fc6cd071c59e73ede669c771f76
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-08-06 17:28:24 +0700

    Added subscription page

--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -1,52 +1,61 @@
-import { NextPage } from 'next';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { queries } from 'graphql/queries';
+import { useSession } from 'next-auth/react';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
-const SubscriptionPage: NextPage = () => {
-  const router = useRouter();
 
-  // On mount, check if user has active subscription
+const Subscription = () => {
+  const { data: session } = useSession();
+  console.log('session',session)
+  const user_query = queries.GET_USER_BY_EMAIL
+  const [userRef, setUserRef] = useState<string | null>(null);
+  const { loading, data } = useQuery<Record<string, any>>(user_query, {
+    variables: { email: session?.user?.email },
+  });
+  
   useEffect(() => {
-    const checkSubscription = async () => {
-      // TODO: Replace with your actual subscription checking logic
-      const hasSubscription = false;
+    if (!loading && data && data.userByEmail) {
+      const userRef = data.userByEmail?.id;
+      console.log('user_ref', userRef);
+      setUserRef(userRef);
+    }
+  }, [loading, data]);
 
-      if (hasSubscription) {
-        // If user has active subscription, redirect to main page
-        router.push('/main');
-      }
-    };
-
-    checkSubscription();
-  }, [router]);
+  if(!userRef){console.log('!userRef',userRef)}
+  
+  const stripeHTML = `
+    <stripe-pricing-table 
+    pricing-table-id="prctbl_1Nc0foIrrtDVRB0pQKFEmNjO" 
+    publishable-key="pk_test_51NNWTwIrrtDVRB0pW9iXxyK6xF3SbJdZ5Ry6hyHEFFDxYDsvajmy2o7inI9C7rnj9yOjyKzPWrejrDBGBP0MvuxM00Zhd5xpez"
+    client-reference-id=${userRef}>
+    </stripe-pricing-table>
+  `;
 
   return (
-    <div className='h-screen flex flex-col justify-center  bg-gray-900'>
-      <div className='mx-auto w-full max-w-lg p-8 rounded-lg backdrop-filter backdrop-blur-md bg-white bg-opacity-20 text-xl'>
-        <h1 className='text-3xl font-bold text-center mb-4 text-gray-200'>Subscription Required</h1>
-        <p className='text-gray-200 mb-4'>
-        Take advantage of our 3-month free trial to enjoy our premium content! You have full control and can cancel your subscription at any moment.
+    <div className="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+      <Head>
+        <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+      </Head>
+      <div className="bg-white p-12 rounded-lg shadow-xl w-3/4 text-center  flex flex-col items-center">
+        <h1 className="text-2xl font-bold mb-4 ">Subscribe to 80K View</h1>
+        <p className="text-gray-700 mb-4 max-w-md">
+          We are on a mission to empower and improve people's life! When you sign up, we are giving you an <span className=' font-bold'>entire year</span> of <span className=' font-bold'>free subscription!</span>
         </p>
-        <span className=' font-extrabold text-red-500'>Important:</span>
-        <p className='text-red-500 font-semibold mb-4'>
-          Please make sure to subscribe with the same email address used for your 80k account.
-        </p>
-        <div className='text-center'>
-          <Link href="https://buy.stripe.com/test_28o2an8ee7a38Xm7ss">
-            <button className='w-full py-3 px-4 text-gray-200 font-bold bg-blue-600 hover:bg-blue-700 active:bg-blue-600 rounded-lg'>Subscribe Now!</button>
-          </Link>
+        <div className='w-full' dangerouslySetInnerHTML={{ __html: stripeHTML }} />
+        <div className="flex flex-col space-y-4">
+          <p>OR</p>
+          <button
+            className="text-blue-600 hover:underline transition"
+            onClick={() => window.location.href = '/'}
+          >
+            Back to Home
+          </button>
         </div>
-        <div className="mt-5 text-center text-xs text-gray-200">
-        Already have an subscription?{' '}
-          <a href="/" className="text-primary-500 hover:text-primary-600">
-            Go to home page
-          </a>
-        .
       </div>
-      </div>
+      
     </div>
   );
 };
 
-export default SubscriptionPage;
+export default Subscription;
