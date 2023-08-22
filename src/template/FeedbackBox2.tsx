daea56b329082a137c035789326e239cfaf243b7
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-08-22 17:05:56 +0700

    Added notes as well

--- src/template/FeedbackBox2.tsx
+++ src/template/FeedbackBox2.tsx
@@ -1,8 +1,5 @@
 import { Session } from 'next-auth';
-import React from 'react';
-// import Modal from 'react-modal';
-
-
+import React, { useState } from 'react';
 
 interface FeedbackBoxProps {
   userData?: UserDataById;
@@ -25,67 +22,35 @@ const FeedbackBox: React.FC<FeedbackBoxProps> = ({
   currentIndex,
   border,
 }) => {
+  const [showNotes, setShowNotes] = useState(false);
 
-  // const [items, setItems] = useState<string[]>([]);
-  // const [modalIsOpen, setModalIsOpen] = useState(false);
-  // const [itemToDelete, setItemToDelete] = useState<number | null>(null);
-  // const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
-
-  // useEffect(() => {
-  //   if (userData && userData[sortedCategoryNames[currentIndex] + "_feedback" as CategoryFeedbackKey]) {
-   
-  //   const feedbacks = [
-  //     (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}` as CategoryKey]?.[0]?.action_plan,
-  //     (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryKey]?.[0]?.action_plan,
-  //     (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryKey]?.[0]?.action_plan,
-  //     // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice1,
-  //     // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice2,
-  //     // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice3,
-  //     // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice4,
-  //     // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice5,
-  //   ];
-  //   const filteredFeedbacks = feedbacks.filter((item): item is string => item !== undefined);
-  //   setItems(filteredFeedbacks);
-  //   setCheckedItems(new Array(feedbacks.length).fill(false));
-  // }
-  // }, [userData, currentIndex, sortedCategoryNames]);
-
-  // const openModal = (index: number) => {
-  //   setItemToDelete(index);
-  //   setModalIsOpen(true);
-  // };
-
-
-  // const handleCheckChange = (position: number) => {
-  //   const updatedCheckedItems = [...checkedItems];
-  //   updatedCheckedItems[position] = !updatedCheckedItems[position];
-  //   setCheckedItems(updatedCheckedItems);
-  // };
-
-  // const closeModal = () => {
-  //   setModalIsOpen(false);
-  //   setItemToDelete(null);
-  // };
-
-  // const handleDelete = () => {
-  //   if(itemToDelete !== null){
-  //     // setItems(items => items.filter((_item, i) => i !== itemToDelete));
-  //   }
-  //   closeModal();
-  // }
   return session ? (
     <>
-      <div className={`w-full rounded-lg shadow-md text-gray-700 bg-white p-6 flex flex-col border-2 ${border}`}>
+      <div className={`w-full rounded-lg shadow-md text-gray-900 bg-white p-6 flex flex-col border-2 ${border}`}>
         <div className='mb-4'>
           <p className='text-lg font-semibold'>
-            Improvement suggestion for this month
+            {showNotes ? "Last Month's Notes:" : "Last Month's Action Plan:"}
           </p>
         </div>
-        <div className='text-base'>
-        {(userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}` as CategoryKey]?.[0]?.action_plan}
+        <div className=''>
+          {showNotes ? (
+            <div className='text-base'>
+              {(userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}` as CategoryKey]?.[0]?.notes}
+            </div>
+          ) : (
+            <div className='text-base'>
+              {(userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}` as CategoryKey]?.[0]?.action_plan}
+            </div>
+          )}
+        </div>
+        <div className="mt-4 text-gray-600 ">
+          {showNotes ? (
+            <button className='hover:underline underline-offset-1 decoration-gray-600 decoration-2' onClick={() => setShowNotes(false)}>Show Action Plan</button>
+          ) : (
+            <button className='hover:underline underline-offset-1 decoration-gray-600 decoration-2' onClick={() => setShowNotes(true)}>Show Notes</button>
+          )}
         </div>
       </div>
-
     </>
   ) : (
     <div className=""></div>
