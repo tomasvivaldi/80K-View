aa6ad802687b805d82292077386940493de64e7c
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-09-30 08:04:16 +0700

    Quick solution for form button confusion

--- src/template/AnswerSection.tsx
+++ src/template/AnswerSection.tsx
@@ -616,9 +616,13 @@ useEffect(() => {
           handleNextClick();
       }}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-800">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
-      </svg>
+      {page === 10 ? (
+          <span className="text-gray-100 font-semibold">Review</span>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-800">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+          </svg>
+        )}
 
     </button>
 
