860766f5a3f0fa25ff64c762d8c14063e0803d39
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-10-14 10:53:17 +0200

    Removed AI feedback

--- src/template/FeedbackBox2.tsx
+++ src/template/FeedbackBox2.tsx
@@ -27,24 +27,24 @@ const FeedbackBox: React.FC<FeedbackBoxProps> = ({
   const [items, setItems] = useState<string[]>([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [itemToDelete, setItemToDelete] = useState<number | null>(null);
-  const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
+  // const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
 
-  useEffect(() => {
-    if (userData && userData[sortedCategoryNames[currentIndex] + "_feedback" as CategoryFeedbackKey]) {
+  // useEffect(() => {
+  //   if (userData && userData[sortedCategoryNames[currentIndex] + "_feedback" as CategoryFeedbackKey]) {
 
-    const feedbacks = [
-      (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.feedback,
-      (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice1,
-      (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice2,
-      (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice3,
-      (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice4,
-      (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice5,
-    ];
-    const filteredFeedbacks = feedbacks.filter((item): item is string => item !== undefined);
-    setItems(filteredFeedbacks);
-    setCheckedItems(new Array(feedbacks.length).fill(false));
-  }
-  }, [userData, currentIndex, sortedCategoryNames]);
+    // const feedbacks = [
+      // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.feedback,
+      // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice1,
+      // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice2,
+      // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice3,
+      // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice4,
+      // (userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}_feedback` as CategoryFeedbackKey]?.[0]?.advice5,
+    // ];
+  //   const filteredFeedbacks = feedbacks.filter((item): item is string => item !== undefined);
+  //   setItems(filteredFeedbacks);
+  //   setCheckedItems(new Array(feedbacks.length).fill(false));
+  // }
+  // }, [userData, currentIndex, sortedCategoryNames]);
 
   // const openModal = (index: number) => {
   //   setItemToDelete(index);
@@ -68,7 +68,7 @@ const FeedbackBox: React.FC<FeedbackBoxProps> = ({
       dark:bg-slate-900/80 dark:shadow-slate-200/5 dark:text-slate-200`}>
         <div className='mb-4 flex flex-row justify-between align-baseline'>
           <p className='text-lg font-semibold'>
-            {showNotes ? "Notes And Action Plan:" : "AI Advice:"}
+            {showNotes ? "Notes:" : "Action Plan:"}
           </p>
           <div className=" text-gray-600 dark:text-slate-400">
           {showNotes ? (
@@ -83,33 +83,37 @@ const FeedbackBox: React.FC<FeedbackBoxProps> = ({
         {showNotes ? (
           <div className='flex flex-col gap-4'>
             <div className='text-base'>
-              <p className=' font-medium'>Notes</p>
+              {/* <p className=' font-medium'>Notes</p> */}
               {(userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}` as CategoryKey]?.[0]?.notes}
             </div>
-            <div className='text-base'>
-              <p className=' font-medium'>Action Plan</p>
-            {(userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}` as CategoryKey]?.[0]?.action_plan}
-            </div>
+
           </div>
           ) : (
-           <ol className='space-y-4'>
-           {items.length > 0 && items.map((item, index) => (index > 0 && userData &&
-             <li key={index} className={`flex items-center space-x-2 ${checkedItems[index] ? 'line-through text-gray-500' : ''}`}>
-               {/* {index > 0 && userData && (
-                 <input type="checkbox" className="form-checkbox h-5 w-5 text-sky-500" checked={checkedItems[index]} onChange={() => handleCheckChange(index)} />
-               )} */}
-               <span className='flex-grow'>{item}</span>
-               {/* {index > 0 && (
-                 <button className=" mx-auto p-1 text-gray-500 hover:text-red-500 focus:outline-none transition duration-150 ease-in-out" onClick={() => openModal(index)}>
-                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
-                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                   </svg>
-                 </button>
-               )}  */}
-             </li>
-           ))}
 
-           </ol>
+            <div className='text-base'>
+              {/* <p className=' font-medium'>Action Plan</p> */}
+            {(userData as UserDataById)?.[`${sortedCategoryNames[currentIndex]}` as CategoryKey]?.[0]?.action_plan}
+            </div>
+
+
+          //  <ol className='space-y-4'>
+          //  {items.length > 0 && items.map((item, index) => (index > 0 && userData &&
+          //    <li key={index} className={`flex items-center space-x-2 ${checkedItems[index] ? 'line-through text-gray-500' : ''}`}>
+          //      {/* {index > 0 && userData && (
+          //        <input type="checkbox" className="form-checkbox h-5 w-5 text-sky-500" checked={checkedItems[index]} onChange={() => handleCheckChange(index)} />
+          //      )} */}
+          //      <span className='flex-grow'>{item}</span>
+          //      {/* {index > 0 && (
+          //        <button className=" mx-auto p-1 text-gray-500 hover:text-red-500 focus:outline-none transition duration-150 ease-in-out" onClick={() => openModal(index)}>
+          //          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+          //            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+          //          </svg>
+          //        </button>
+          //      )}  */}
+          //    </li>
+          //  ))}
+
+          //  </ol>
           )}
          </div>
       </div>
