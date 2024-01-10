67b5470cc712c5acc5135a0fcd999ce3219c28fc
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2024-01-10 22:14:12 +0530

    Google analitics implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ import { ThemeProvider } from 'next-themes';
 
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import Script from 'next/script';
 
 
 const MyApp = ({
@@ -21,6 +22,20 @@ const MyApp = ({
       <Toaster />
       <ThemeProvider attribute="class">
       <LocalizationProvider dateAdapter={AdapterDateFns}>
+
+      <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-K50BW4JH06`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-K50BW4JH06');
+          `}
+      </Script>
+
         <Component {...pageProps} />
         </LocalizationProvider>
       </ThemeProvider>
