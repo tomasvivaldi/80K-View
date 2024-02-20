acfd5b308c22be55934637f59471e517e0745573
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2024-02-20 08:17:52 +0400

    webhook authoziration test

--- src/pages/api/wix/wix-webhook.ts
+++ src/pages/api/wix/wix-webhook.ts
@@ -1,9 +1,61 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'jsonwebtoken';
+// import { buffer } from 'micro';
+// import { UPDATE_USER_SUBSCRIPTION } from 'graphql/mutations';
+// import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+// import fetch from 'cross-fetch';
+
+
+// const httpLink = createHttpLink({ 
+//   uri: process.env.STEPZEN_ENDPOINT,
+//   fetch
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   return {
+//     headers: {
+//       ...headers,
+//       Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 
 const wixWebhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         
   console.log(`Webhook hit with method: ${req.method}`);
   if (req.method === 'POST') {
+
+
+    const token = req.headers['digest']; // Assuming 'digest' contains the JWT
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+// Ensure token is a string
+const tokenString = Array.isArray(token) ? token[0] : token;
+
+try {
+  const decoded = jwt.verify(tokenString!, process.env.WIX_PUBLIC_KEY!);
+
+    console.log('Verified JWT:', decoded);
+
+
+
+
+
     console.log('Received Wix Webhook:', req.body);
 
     // Log detailed information
@@ -41,8 +93,10 @@ const wixWebhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     console.log(`Contact Address Line 1: ${contact?.address?.addressLine}, Contact Address Line 2: ${contact?.address?.addressLine2}`);
     console.log(`Contact Address Subdivision: ${contact?.address?.subdivision}`);
 
-    // Send a response back to Wix to acknowledge receipt
-    res.status(200).json({ message: 'Webhook received successfully' });
+    res.status(200).json({ message: 'Webhook received and verified' });
+  } catch (error) {
+      return res.status(401).json({ message: 'Failed to authenticate token' });
+  }
   } else {
     // If the request is not a POST, send a 405 Method Not Allowed response
     res.setHeader('Allow', 'POST');
