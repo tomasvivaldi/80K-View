57cdeadfbaa0e244903da2de706048eb150c458a
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2024-02-23 07:58:00 +0400

    fix errors

--- src/pages/api/wix/wix-webhook.ts
+++ src/pages/api/wix/wix-webhook.ts
@@ -1,12 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 // import jwt from 'jsonwebtoken';
-
-import { UPDATE_USER_SUBSCRIPTION } from 'graphql/mutations';
-
+import { ADD_WIX_SUBSCRIPTION } from 'graphql/mutations';
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import fetch from 'cross-fetch';
-
 
 export const config = {
   api: {
@@ -14,11 +11,7 @@ export const config = {
   },
 };
 
-
-
 const wixWebhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-
-
   const httpLink = createHttpLink({ 
     uri: process.env.STEPZEN_ENDPOINT,
     fetch
@@ -42,39 +35,31 @@ if (req.method === 'POST') {
   console.log('Webhook hit with method: POST');
   // console.log('req',req);
 
-
-  // Assuming the JWT is sent directly in the body or in a specific field
-  const token = req.body; // Adjust this if the JWT is nested within the body
-console.log('body',req.body);
   // Decode JWT without verifying
   try {
-    const wixSubscription = token?.body
-console.log("wixSubscription",wixSubscription)
+    const wixSubscription = req.body
 
     if (wixSubscription!) {
       try {
         const response = await client.mutate({
-          mutation: UPDATE_USER_SUBSCRIPTION,
-          variables: { email: wixSubscription, isActive: true },
+          mutation: ADD_WIX_SUBSCRIPTION,
+          variables: {
+            email: wixSubscription.contact.email,
+            wix_order_id: wixSubscription.plan_order_id,
+            data: wixSubscription,
+            recorded_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+          },
         });
       
         console.log(`Response:`, response);
         
-        if (response.data?.updateUsers) {
-          console.log(`User subscription status updated!`, response.data.updateUsers);
-        } else {
-          console.error(`No user found with email: ${wixSubscription}`);
-        }
       } catch (err) {
         console.error(`Failed to update user subscription status: ${err}`);
       }
     }
 
-    console.log('Successfully logged to database!');
-
-
-
-    res.status(200).json({ message: 'webhook received' });
+    res.status(200).json({ message: 'Webhook successfully received' });
   } catch (error) {
     console.error("JWT decoding failed:", error);
 
